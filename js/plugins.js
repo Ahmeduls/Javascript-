c6445b013e054699e30b90efd41b851008bc6332
Md. Ahmedul Hoque <30325346+Ahmeduls@users.noreply.github.com>
2017-10-11 20:55:58 +0600

    Add files via upload

---
+++ js/plugins.js
@@ -0,0 +1,35 @@
+//if (number > 2) {
+//    document.write("Your entered number is grater than 2.");
+//} else {
+//    document.write("Your entered number is less than 2.");
+//}
+
+
+
+//var number;
+//        number = Number(prompt("Please enter a number"));
+//
+//        if (number % 2 == 0) {
+//            alert(number + " Even Number.");
+//        } else {
+//            alert(number + " Odd Number.");
+//        }
+
+
+
+//var marks;
+//        marks = Number(prompt("Please Enter Your Marks"));
+//
+//        if (marks >= 80 && marks <= 100) {
+//            document.write("You got A+");
+//        } else if (marks >= 70 && marks <= 79) {
+//            alert("Your got A");
+//        } else if (marks >= 60 && marks <= 69) {
+//            alert("You got B");
+//        } else if (marks >= 50 && marks <= 59) {
+//            alert("you got C");
+//        } else if (marks > 0 && marks <= 50) {
+//            alert("You Got Fail");
+//        } else {
+//            alert("Your have enter Invelid Number")
+//        }
